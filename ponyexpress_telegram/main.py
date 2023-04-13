da428e65fda861bcc3703aa4eec7170ffc744646
Philipp Kessling <p.kessling@leibniz-hbi.de>
2023-04-13 16:42:34 +0200

    feat: refactor scraper

--- ponyexpress_telegram/main.py
+++ ponyexpress_telegram/main.py
@@ -16,9 +16,13 @@ log_levels = {0: "ERROR", 1: "WARNING", 2: "INFO", 3: "DEBUG", 4: "TRACE"}
 @click.argument("names", type=str, nargs=-1)
 @click.option("--messages-output", "-m", type=click.File("a"), default="-", help="")
 @click.option("--users-output", "-u", type=click.File("a"), default="-")
+@click.option("--prepare-edges", "-p", is_flag=True)
 @click.option("--log-file", "-l", type=click.Path())
-@click.option('-v', '--verbose', count=True, default=0)
-def cli(names: List[str], messages_output, users_output, log_file: Path, verbose: int) -> None:
+@click.option("-v", "--verbose", count=True, default=0)
+def cli(
+    names: List[str], messages_output, users_output, prepare_edges: bool, log_file: Path,
+    verbose: int
+) -> None:
     """Scrape a Telegram Channel"""
     log.remove()  # remove all default loggers
     # and set the logging level according to user input
@@ -26,7 +30,7 @@ def cli(names: List[str], messages_output, users_output, log_file: Path, verbose
     _sink_ = log_file or sys.stdout  # if a log_file is specified omit logging to stdout
     log.add(_sink_, level=_log_level_)
 
-    edges, users = telegram_connector(names)
+    edges, users = telegram_connector(names, {"prepare_edges": prepare_edges})
 
     edges.to_json(messages_output, orient="records", lines=True)
     users.to_json(users_output, orient="records", lines=True)
--- ponyexpress_telegram/telegram.py
+++ ponyexpress_telegram/telegram.py
@@ -6,12 +6,12 @@ ToDo:
 """
 
 from functools import reduce
-from typing import List, Optional, Tuple
+from typing import List, Optional, Tuple, Dict, Any, Callable
 
 import pandas as pd
 import requests
 from loguru import logger as log
-from lxml import html
+from lxml import html, etree
 
 message_paths = {
     # pylint: disable=C0301
@@ -22,19 +22,18 @@ message_paths = {
     "from_author": ".//span[@class='tgme_widget_message_from_author']/descendant-or-self::*/text()",
     "text": "./div[contains(@class, 'tgme_widget_message_text')]/descendant-or-self::*/text()",
     "link": ".//div[contains(@class, 'tgme_widget_message_text')]//a/@href",
-    "reply_to_user" : ".//a[@class='tgme_widget_message_reply']//span[@class='tgme_widget_message_author_name'/descendant-or-self::*/text()]",
-    "reply_to_text" : ".//a[@class='tgme_widget_message_reply']//div[@class='tgme_widget_message_text']/descendant-or-self::*/text()",
-    "reply_to_link" : ".//a[@class='tgme_widget_message_reply']/@href",
+    "reply_to_user": ".//a[@class='tgme_widget_message_reply']//span[@class='tgme_widget_message_author_name'/descendant-or-self::*/text()]",
+    "reply_to_text": ".//a[@class='tgme_widget_message_reply']//div[@class='tgme_widget_message_text']/descendant-or-self::*/text()",
+    "reply_to_link": ".//a[@class='tgme_widget_message_reply']/@href",
     "image_url": "./a[contains(@class, 'tgme_widget_message_photo_wrap')]/@style",
     "forwarded_message_url": ".//a[@class='tgme_widget_message_forwarded_from_name']/@href",
     "forwarded_message_user": ".//a[@class='tgme_widget_message_forwarded_from_name']/descendant-or-self::*/text()",
     #       "poll_question" : ".//div[@class='tgme_widget_message_poll_question']/descendant-or-self::*/text()",
     #       "poll_options_text" : ".//div[@class='tgme_widget_message_poll_option_value']/descendant-or-self::*/text()",
     #       "poll_options_percent" : ".//div[@class='tgme_widget_message_poll_option_percent']/descendant-or-self::*/text()",
-    "video_url" : ".//video[contains(@class, 'tgme_widget_message_video')]/@src",
-    "video_duration" : ".//time[contains(@class, 'message_video_duration')]/descendant-or-self::*/text()"
+    "video_url": ".//video[contains(@class, 'tgme_widget_message_video')]/@src",
+    "video_duration": ".//time[contains(@class, 'message_video_duration')]/descendant-or-self::*/text()",
 }
-
 
 user_paths = {
     # pylint: disable=C0301
@@ -59,85 +58,88 @@ def parse_tg_number_string(num: str) -> float:
     return 0.0
 
 
-def get_messages(page) -> pd.DataFrame:
+def extract_from(tree, xpaths):
+    """extracts data from a lxml tree based on a dictionary of xpaths"""
+    return {name: tree.xpath(xpath) for name, xpath in xpaths.items()}
+
+
+def cleanup(data_dict: Dict[str, Any], rules: Dict[Callable, Callable]) -> Dict[str, Any]:
+    """Rule-based data cleaning.
+
+    Args:
+        data_dict: the data to be cleaned
+        rules: the rules to be applied, whereas a ruleset is a dictionary with predicates as keys
+            and cleaning functions as values. The predicate is a function that takes the key and
+            value of the data_dict and returns a boolean. If the predicate returns True, the
+            corresponding cleaning function is applied to the value.
+    Returns:
+        Dict[str, Any] : the cleaned data
+    """
+    return {
+        key: reduce(lambda _value, ruleset: ruleset[1](_value) if ruleset[0](key, _value) is
+        True else _value, rules.items(), value)
+        for key, value in data_dict.items()
+    }
+
+
+def get_messages(page: etree.ElementTree) -> pd.DataFrame:
     """get Telegram messages from a HTML document
 
     Args:
-      page: lxml.tree : the parsed HTML document
+      page: the parsed HTML document
 
     Returns:
-
-    the parsed messages, where the columns are based on the
-    keys of the messages_paths-dictionary as a ``pd.DataFrame``.
+      the parsed messages, where the columns are based on the
+      keys of the messages_paths-dictionary as a ``pd.DataFrame``.
     """
 
-    def extract_multiple2(tree, xpaths):
-        for name, xpath in xpaths.items():
-            # first post-processing steps
-
-            parsed = tree.xpath(xpath)
-            # log.debug(f"Parsing {name} with value: {parsed}.")
-
-            if name == "link":
-                data = ",".join(parsed)
-            else:
-                data = "".join(parsed)
-                if name in ["views"]:
-                    data = parse_tg_number_string(data)
-                # if name == "image_url":
-                #     data = re.findall(r"(?<=url\(').+(?=')", data)
-            if data == "":
-                data = None
-            yield pd.Series(
-                data=data, name=name, dtype="object" if isinstance(data, str) else float
-            )
-
     base_xpath = "//div[@class='tgme_widget_message_bubble']"
-
     messages_html = page.xpath(base_xpath)
+    rules = {
+        lambda key, value: key == "text": lambda x: "\n".join(x),
+        lambda key, value: key == "datetime": lambda x: pd.to_datetime(x[0]),
+        lambda key, value: "count" in key: lambda x: parse_tg_number_string(x[0]),
+        lambda key, value: key == "views": lambda x: parse_tg_number_string(x[0]),
+        lambda key, value: isinstance(value, list): lambda x: x[0] if len(x) > 0 else None,
+        lambda key, value: True: lambda x: x if x != "" else None,
+    }
     try:
-        data = pd.concat(
-            [
-                pd.concat(extract_multiple2(_, message_paths), axis=1)
-                for _ in messages_html
-            ],
-            axis=0,
-            ignore_index=True,
-        )
+        data = [extract_from(_, message_paths) for _ in messages_html]
     except ValueError:
         # If this code is executed, we could not obtain data from the TG-page
         return pd.DataFrame()
-    data[["handle", "post_number"]] = data.post_id.str.split("/", n=1, expand=True)
 
+    data = [cleanup(_, rules) for _ in data]
+
+    data = pd.DataFrame(data)
+    data[["handle", "post_number"]] = data.post_id.str.split("/", n=1, expand=True)
     return data
 
 
 def get_user(page) -> pd.DataFrame:
-    """get Telegram user data from a HTML document
+    """get Telegram user data from an HTML document
 
     Args:
-      page: lxml.tree : the parsed HTML document
+      page: the parsed HTML document
 
     Returns:
-
-    The parsed messages in a ``pd.DataFrame``, where the columns are based on the
-    keys of the user_paths-dictionary.
+      The parsed messages in a ``pd.DataFrame``, where the columns are based on the
+       keys of the user_paths-dictionary.
     """
-
-    def extract_multiple(tree, xpaths):
-        for name, xpath in xpaths.items():
-            data = "".join(tree.xpath(xpath))
-            if "count" in name:
-                data = parse_tg_number_string(data)
-            if data == "":
-                data = None
-            yield pd.Series(
-                data=data, name=name, dtype="object" if isinstance(data, str) else float
-            )
+    rules = {
+        lambda key, value: isinstance(value, list): lambda x: x[0] if len(x) > 0 else None,
+        lambda key, value: key == "name": lambda x: x.replace("@", ""),
+        lambda key, value: "count" in key: lambda x: parse_tg_number_string(x),
+        lambda key, value: True: lambda x: x if x != "" else None,
+    }
 
     try:
-        data = pd.concat(extract_multiple(page, user_paths), axis=1)
-        data["name"] = data["name"].str.replace("@", "")
+        data = extract_from(page, user_paths)
+        data = cleanup(data, rules)
+
+        log.debug(f"Extracted user data: {data}")
+
+        data = pd.DataFrame([data])
     except ValueError:
         # If this code is executed, we could not obtain data from the TG-page
         return pd.DataFrame()
@@ -147,7 +149,28 @@ def get_user(page) -> pd.DataFrame:
     return data
 
 
-def telegram_connector(node_names: List[str]) -> Tuple[pd.DataFrame, pd.DataFrame]:
+def get_node(node_name: str) -> Optional[Tuple[pd.DataFrame, pd.DataFrame]]:
+    visit_url = f"https://t.me/s/{node_name}"
+
+    resp = requests.get(visit_url)
+
+    log.debug(f"Visited node {node_name} with status: {resp.status_code}")
+
+    if resp.status_code != 200:
+        log.warning(f"{visit_url} did not succeed with status {resp.status_code}")
+
+        return pd.DataFrame(), pd.DataFrame([{"name": node_name, "handle": node_name}])
+
+    else:
+        html_source = html.fromstring(resp.content)
+        messages = get_messages(html_source)
+        nodes = get_user(html_source)
+
+        return messages, nodes
+
+
+def telegram_connector(node_names: List[str], configuration: Dict[str, Any]) -> Tuple[pd.DataFrame,
+pd.DataFrame]:
     """for a list of handles scrape their public telegram channels
 
     That is, if there is a public channel present for the specified handles.
@@ -158,41 +181,7 @@ def telegram_connector(node_names: List[str]) -> Tuple[pd.DataFrame, pd.DataFram
     Returns:
      edges, nodes in a Tuple[pd.DataFrame, pd.DataFrame].
     """
-
-    def get_node(node_name: str) -> Optional[Tuple[pd.DataFrame, pd.DataFrame]]:
-        visit_url = f"https://t.me/s/{node_name}"
-        resp = requests.get(visit_url)
-        log.debug(f"Visited node {node_name} with status: {resp.status_code}")
-
-        if resp.status_code == 302:
-            log.warning(f"{visit_url} throw an error")
-        if resp.status_code == 200:
-            html_source = html.fromstring(resp.content)
-            messages = get_messages(html_source)
-
-            nodes = get_user(html_source)
-            log.debug(f"Parsed userdata for {node_name}:\n{nodes}\n")
-            # except Exception:
-            #     log.warning(f"Parsing failed for {node_name}")
-            #     return None
-            if len(messages) > 0:
-                edges = messages.loc[~messages["forwarded_message_url"].isnull(), :].copy()
-
-                if len(edges) > 0:
-                    edges.loc[:, "source"] = edges.loc[:, "handle"]
-                    edges.loc[:, "target"] = edges.forwarded_message_url.str.extract(
-                        r"([\w_]+)(?=\/\d+)"
-                    )
-
-                log.debug(f"Parsed message for {node_name}:\n{edges}\n")
-                log.debug(f"Retrieved {len(edges)} edges for {node_name}")
-
-                return edges, nodes
-
-        log.warning(f"parsing failed for {node_name}")
-        return pd.DataFrame(), pd.DataFrame()
-
-    def reduce_returns(
+    def _reduce_returns(
             carry: Tuple[pd.DataFrame, pd.DataFrame],
             value: Tuple[pd.DataFrame, pd.DataFrame],
     ):
@@ -200,9 +189,20 @@ def telegram_connector(node_names: List[str]) -> Tuple[pd.DataFrame, pd.DataFram
 
     _ret_ = [get_node(_) for _ in node_names]
     _ret_ = [_ for _ in _ret_ if _ is not None]
-
-    log.debug(f"TG Connector found: {_ret_}")
-
     if len(_ret_) <= 0:
         return pd.DataFrame(), pd.DataFrame()
-    return reduce(reduce_returns, _ret_)
+    if configuration.get("prepare_edges", False):
+        if len(_ret_[0]) > 0:
+            edges = _ret_[0].loc[
+                    ~_ret_[0]["forwarded_message_url"].isnull(), :
+                    ].copy()
+
+            if len(edges) > 0:
+                edges.loc[:, "source"] = edges.loc[:, "handle"]
+                edges.loc[:, "target"] = edges.forwarded_message_url.str.extract(
+                    r"([\w_]+)(?=\/\d+)"
+                )
+                edges.loc[:, "type"] = "forward"
+                _ret_[0] = edges
+
+    return reduce(_reduce_returns, _ret_)
